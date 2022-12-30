cf17e74056e2d5b94ee4cc38bb978f9e3b1f698a
shivam vora <vorashivam0@gmail.com>
2022-12-30 23:03:46 +0530

    update changes for env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import Connection from './database/db.js';
 const app = express();
-dotenv.config();
+dotenv.config({ path: "./envs/.env" });
 
 const PORT = 8000;
 const USERNAME = process.env.DB_USERNAME;
