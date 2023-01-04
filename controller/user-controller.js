4183d2683029cce269bc01df8615f351102976ac
shivam vora <vorashivam0@gmail.com>
2023-01-04 23:39:02 +0530

    update code for login and signup backend side

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,7 +1,48 @@
-export const userSignup = (req, res) => {
-    try {
-        console.log(req.body);
-    } catch (error) {
+import User from '../model/user-schema.js';
 
+export const userSignup = async (req, res) => {
+    try {
+
+        const exist = await User.findOne({ username: req.body.username });
+        if (exist) {
+            return res.status(401).json({
+                error: 'User already exists'
+            })
+        }
+        const user = req.body;
+        const newUser = new User(user);
+        await newUser.save();
+
+        res.status(201).json({
+            message: 'User created successfully',
+            data: user
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+}
+
+export const userLogin = async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        let user = await User.findOne({ username: username, password: password })
+        if (user) {
+            return res.status(200).json({
+                message: 'User logged in successfully',
+                data: user
+            })
+        } else {
+            return res.status(401).json({
+                error: 'User not found'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
     }
 }
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { userSignup } from '../controller/user-controller.js';
+import { userLogin, userSignup } from '../controller/user-controller.js';
 const router = express.Router();
 
 router.post('/signup', userSignup);
+router.post('/login', userLogin)
 
 export default router;
