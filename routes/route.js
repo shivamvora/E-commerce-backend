7f8bb7ac7881e04f06f65b4e94a2f6d08ef9f97e
shivam vora <vorashivam0@gmail.com>
2023-01-08 12:54:07 +0530

    setup get product api

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import { getProducts } from '../controller/product-controller.js';
 import { userLogin, userSignup } from '../controller/user-controller.js';
 const router = express.Router();
 
 router.post('/signup', userSignup);
-router.post('/login', userLogin)
+router.post('/login', userLogin);
+router.get('/products', getProducts);
 
 export default router;
